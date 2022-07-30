9ca54759e5acd2c64ba52a8ad3a5c866958a93e8
chaitalinandkar <chaitalinandkar@gmail.com>
2022-07-29 19:10:49 -0700

    Styled the page with CSS and added audio to the word

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Meaning.css"
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <h3 className="text-capitalize">{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-          <p>Definition : {definition.definition}</p>
+          <p className="fw-semibold">Definition : {definition.definition}</p>
           <em><p>Example : {definition.example}</p></em>
         </div>
         )
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Meaning from "./Meaning";
+import "./Results.css"
 
 export default function Results(props) {
- 
+
   if (props.result) {
     return (
       <div className="Results">
-        <h4>{props.result.word}</h4>
+        <div className="searchedWord">
+          <h4 className="fs-1 text-capitalize">{props.result.word}</h4>
+          <h4><a href={props.result.phonetics[0].audio} target="_blank"><i class="fa-solid fa-circle-play me-3"></i></a>{props.result.phonetic}</h4>
+        </div>
         
         {props.result.meanings.map(function (meaning, index) {
           return (
